2fb576772634c7b0f6c40436026f2246d250aa85
swartdc <swartdc@rose-hulman.edu>
2018-12-11 21:09:18 -0500

    DONE

--- src/problem4.py
+++ src/problem4.py
@@ -3,8 +3,8 @@ Exam 1, problem 4.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Derrick Swart.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -68,11 +68,37 @@ def problem4(number_of_stairs, step_size, starting_point, window):
       :type window:            rg.RoseWindow
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # IMPORTANT: For PARTIAL CREDIT, you can draw just the black "bottoms"
     #            of the stair steps.
     # -------------------------------------------------------------------------
+    first_point_vert = starting_point
+    second_Point_vert = rg.Point(starting_point.x, starting_point.y - step_size)
+    first_point_hori = second_Point_vert
+    second_Point_hori = rg.Point(first_point_hori.x + step_size, first_point_hori.y)
+    point1 = starting_point
+    point1.attach_to(window)
+    for k in range (number_of_stairs):
+        line_vert = rg.Line(first_point_vert, second_Point_vert)
+        line_vert.color = 'magenta'
+        line_vert.thickness = 3
+
+        line_hori = rg.Line(first_point_hori, second_Point_hori)
+        line_hori.color = 'black'
+        line_hori.thickness = 3
+        line_hori.attach_to(window)
+        line_vert.attach_to(window)
+
+        first_point_vert = second_Point_hori
+        second_Point_vert = rg.Point(first_point_vert.x, first_point_vert.y - step_size)
+        first_point_hori =second_Point_vert
+        second_Point_hori = rg.Point(first_point_hori.x + step_size, first_point_hori.y)
+
+    point = rg.Point(second_Point_hori.x - step_size, second_Point_hori.y + step_size)
+    point.attach_to(window)
+    window.render()
+
 
 
 # -----------------------------------------------------------------------------
